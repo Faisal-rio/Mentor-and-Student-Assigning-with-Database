b04a486e7396b691095ed50910dcc4a7f7dc4bad
Faisal-rio <faisal.sfzubaida@gmail.com>
2024-09-11 04:46:07 -0700

    commit

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mentorship';
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1); // Stop the app if DB connection fails
